54b91fce0076a4f8bfdc9352b0a4d5c92e46a9c7
kpw <rayco@majar.com>
2009-10-02 00:03:40 +0300

    31.10.08

--- SQL/auto_broadcast.sql
+++ SQL/auto_broadcast.sql
@@ -5,4 +5,4 @@ PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8; 
 
 INSERT IGNORE INTO mangos_string (entry, content_default, content_loc1, content_loc2, content_loc3, content_loc4, content_loc5, content_loc6, content_loc7, content_loc8)
-VALUES (1001, '|cffffff00[|c0007776Autobroadcast|cffffff00]: |cFFF222FF%s|r', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+VALUES (2001, '|cffffff00[|c00077766Autobroadcast|cffffff00]: |cFFF222FF%s|r', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
