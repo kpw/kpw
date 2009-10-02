d1df279529db5cd657d0a5798e55ae696d014b7b
kpw <rayco@majar.com>
2009-10-02 23:00:09 +0300

    6832_740

--- ScriptDev2/To work/luker.sql
+++
@@ -1,3 +0,0 @@
-UPDATE `creature_template` SET `ScriptName` = 'boss_the_lurker_below' WHERE `creature_template`.`entry` =21217 ;
-UPDATE `creature_template` SET `ScriptName`='mob_underbog_colossus' WHERE (`entry`='21251');
-UPDATE `creature_template` SET `InhabitType`='3' WHERE (`entry`='21217');
